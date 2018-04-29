d219a73a640e33d560d8cde95280a26ea1a03ef4
lhsang <lehoangsangcpp@gmail.com>
2018-04-29 11:21:38 +0700

    update 1

---
+++ PhanMemXemPhimSongNgu/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace PhanMemXemPhimSongNgu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnchoosefile = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(3, 76);
+            this.axWindowsMediaPlayer1.Margin = new System.Windows.Forms.Padding(4);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(276, 240);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(4, 38);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(367, 22);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnchoosefile
+            // 
+            this.btnchoosefile.Location = new System.Drawing.Point(429, 34);
+            this.btnchoosefile.Margin = new System.Windows.Forms.Padding(4);
+            this.btnchoosefile.Name = "btnchoosefile";
+            this.btnchoosefile.Size = new System.Drawing.Size(100, 28);
+            this.btnchoosefile.TabIndex = 2;
+            this.btnchoosefile.Text = "Choose File";
+            this.btnchoosefile.UseVisualStyleBackColor = true;
+            this.btnchoosefile.Click += new System.EventHandler(this.btnchoosefile_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(429, 94);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 28);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(429, 159);
+            this.btnPause.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(100, 28);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(429, 242);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(349, 74);
+            this.txt1.TabIndex = 5;
+            this.txt1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 441);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnchoosefile);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnchoosefile;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PhanMemXemPhimSongNgu/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Globalization;
+namespace PhanMemXemPhimSongNgu
+{
+    public partial class Form1 : Form
+    {
+        static int index=0;
+        static long timeStart = 0;
+        static long timeEnd = 0;
+        static double temp;
+        static string sub;
+        static List<Match> list =new List<Match>();
+        private static Regex unit = new Regex(
+           @"(?<sequence>\d+)\r\n(?<start>\d{2}\:\d{2}\:\d{2},\d{3}) --\> (?<end>\d{2}\:\d{2}\:\d{2},\d{3})\r\n(?<text>[\s\S]*?\r\n\r\n)",
+           RegexOptions.Compiled | RegexOptions.ECMAScript);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnchoosefile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfileDialog1 = new OpenFileDialog();
+            if (openfileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.txtPath.Text = openfileDialog1.FileName;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = this.txtPath.Text;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+            readData();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(temp==0)
+            {
+                this.txt1.Text = "Sang dep trai";
+            }
+            else   this.txt1.Text = list[index].Groups[4].Value;
+        }
+
+        static void readData()
+        {   
+            using (StreamReader r = new StreamReader("abc.srt"))
+            {
+                string line,ans = "";
+                while ((line = r.ReadLine()) != null )
+                {
+                    if (line == "")
+                    {
+                        ans = ans + "\r\n";
+                        Match m = unit.Match(ans);
+                        if (m.Success)
+                            list.Add(m);
+                        ans = "";
+                    }
+                    else ans = ans + line + "\r\n";
+                }
+            }
+        }
+
+         static bool isNext()
+        {
+            if (temp >= timeStart && temp <= timeEnd)
+                return false;
+            else
+            {
+                index++;
+                timeStart = convertMs(list[index].Groups[2].Value);
+                timeEnd = convertMs(list[index].Groups[3].Value);
+
+                if (temp >= timeStart && temp <= timeEnd)
+                    return false;
+            }
+            return false;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            temp = 1000 * axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+            timeStart = convertMs(list[index].Groups[2].Value);
+            timeEnd = convertMs(list[index].Groups[3].Value);
+            textBox1_TextChanged(sender, e);
+            timer1.Start();
+        }
+        static long convertMs(string time)
+        {
+            long ans = 0;
+            string h, m, s, ms;
+
+            h = time.Substring(0, 2);
+            m = time.Substring(3, 2);
+            s = time.Substring(6, 2);
+            m = time.Substring(9, 3);
+
+            ans = Int32.Parse(h) * 60 * 60 * 1000 + Int32.Parse(m) * 60 * 1000 + Int32.Parse(s) * 1000 + Int32.Parse(m);
+            return ans;
+        }
+    }
+}
